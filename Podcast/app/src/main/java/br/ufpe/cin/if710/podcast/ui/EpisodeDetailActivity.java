05bb767f5c9ac2be007f65b39634d73016fabced
Arthur Freitas <abf2@cin.ufpe.br>
2017-10-15 23:23:43 -0200

    Remove link from descriptions screen

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -13,7 +13,6 @@ import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 public class EpisodeDetailActivity extends Activity {
 
     private TextView title;
-    private TextView link;
     private TextView date;
     private TextView description;
     private TextView downloadLink;
@@ -24,7 +23,6 @@ public class EpisodeDetailActivity extends Activity {
         setContentView(R.layout.activity_episode_detail);
 
         title = findViewById(R.id.ep_title);
-        link = findViewById(R.id.ep_link);
         date = findViewById(R.id.ep_date);
         description = findViewById(R.id.ep_description);
         downloadLink = findViewById(R.id.ep_downloadLink);
@@ -32,7 +30,6 @@ public class EpisodeDetailActivity extends Activity {
         //seta os valores da tela de detalhos do ep com o obj passado no intent
         ItemFeed item = (ItemFeed) getIntent().getSerializableExtra("item");
         title.setText("Title: "+ item.getTitle());
-        link.setText("Link: "+ item.getLink());
         date.setText("Date: "+ item.getPubDate());
         description.setText("Description: " + item.getDescription());
         downloadLink.setText("Download Link: " + item.getDownloadLink());
